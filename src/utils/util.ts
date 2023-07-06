082c4bc0d2101fddcd2d6812bb6792a40d9f3c64
Anthony Lopez <anthodev1195@gmail.com>
2023-07-06 11:27:58 -0500

    set endpoint pagination ok

---
+++ src/utils/util.ts
@@ -0,0 +1,13 @@
+import { Service } from "../interfaces/service.ts";
+
+export const chunkArray = (array: Service[], chunkSize: number) => {
+  const chunks: Service[][] = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunks.push(array.slice(index, index + chunkSize));
+    index += chunkSize;
+  }
+
+  return chunks;
+};
