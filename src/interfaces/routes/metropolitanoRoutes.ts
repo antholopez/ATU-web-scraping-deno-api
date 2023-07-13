dc732d23624c32e67542a36203df9ccab6b096d6
Anthony Lopez <anthodev1195@gmail.com>
2023-07-13 14:53:59 -0500

    add: configuration HA ok

--- src/interfaces/routes/metropolitanoRoutes.ts
+++ src/interfaces/routes/metropolitanoRoutes.ts
@@ -1,6 +1,6 @@
 import { ServiceRepositoryImpl } from "../../metropolitano/services/infrastructure/repositories/service.repository.ts";
 import MetropolitanoServicesController from "./../controllers/metropolitanoController.ts";
-import { Context, Hono } from "https://deno.land/x/hono@v3.2.3/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v3.2.3/mod.ts";
 
 const metropolitanoRouter = new Hono();
 
