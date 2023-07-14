26bcf91ad98c64b4912b0f3a0e3cb29bddd425be
Anthony Lopez <anthodev1195@gmail.com>
2023-07-14 16:22:01 -0500

    fix ok

--- src/cron/job.ts
+++ src/cron/job.ts
@@ -1,8 +1,5 @@
 import { AtuScrapingService } from "./../metropolitano/services/infrastructure/externalServices/atuScrapingService.ts";
-import {
-  daily,
-  everyMinute,
-} from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
+import { daily } from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
 import * as log from "https://deno.land/std@0.190.0/log/mod.ts";
 import { DenoKVServiceRepository } from "../metropolitano/services/infrastructure/repositories/DenoKVServiceRepository.ts";
 import { DenoKVConnection } from "../shared/infrastructure/DenoKVConnection.ts";
@@ -24,7 +21,7 @@ const setWriteDBData = async () => {
 };
 await setWriteDBData();
 
-everyMinute(async () => {
+daily(async () => {
   log.info("Before job instantiation");
   await setWriteDBData();
   log.info("After job instantiation");
